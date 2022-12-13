b796130c922524e7301b5cfe7b30f7fa435f9960
MupetCol <65467881+MupetCol@users.noreply.github.com>
2022-12-12 23:41:28 -0500

    Collision tweaked

--- Assets/ChangeParentSorLayer.cs
+++ Assets/ChangeParentSorLayer.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 public class ChangeParentSorLayer : MonoBehaviour
 {
 	[SerializeField] private int _newSortOrder;
+	[SerializeField] private int _newLayer;
+
+	[SerializeField] private bool _changeCollision;
+
 	[SerializeField] private SpriteRenderer _renderer;
+
+	private void Awake()
+	{
+		_renderer = transform.parent.parent.GetComponent<SpriteRenderer>();
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.tag == "Player")
 		{
 			_renderer.sortingOrder = _newSortOrder;
+
+			if(_changeCollision)
+			_renderer.transform.gameObject.layer = _newLayer;
 		}
 	}
 }
